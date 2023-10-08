0efe1f676a14471241c3d2731bdd6045e27125be
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 12:25:32 +0300

    Update userHandler.go

--- server/handlers/userHandler.go
+++ server/handlers/userHandler.go
@@ -1,24 +1,89 @@
 go
 package handlers
 
-// import necessary packages
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type UserHandler struct {
-	// Define any dependencies or services required by the user handler
-	// Example: db *sql.DB
+	db *sql.DB
 }
 
-// Define methods for handling user operations
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+func NewUserHandler(db *sql.DB) *UserHandler {
+	return &UserHandler{db}
+}
 
 func (uh *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
-	// Logic for creating a new user
-	// Example: receive data from request body, validate inputs, insert into database
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Failed to read request body: %v", err)
+		return
+	}
+
+	// TODO: Validate inputs
+
+	stmt, err := uh.db.Prepare("INSERT INTO users(id, username, email) VALUES($1, $2, $3)")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Failed to prepare insert statement: %v", err)
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(user.ID, user.Username, user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Failed to insert user into database: %v", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "User created successfully")
 }
 
 func (uh *UserHandler) GetUser(w http.ResponseWriter, r *http.Request) {
-	// Logic for retrieving a user
-	// Example: read user ID from request parameters, query database for user details, send JSON response
+	params := mux.Vars(r)
+	userID := params["id"]
+
+	// TODO: Validate user ID
+
+	var user User
+	row := uh.db.QueryRow("SELECT id, username, email FROM users WHERE id = $1", userID)
+	err := row.Scan(&user.ID, &user.Username, &user.Email)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "User not found")
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Failed to retrieve user: %v", err)
+		}
+		return
+	}
+
+	jsonBytes, err := json.Marshal(user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Failed to serialize user: %v", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
 }
 
-// Add more methods as per your application requirements
-
+// more methods can be added here
