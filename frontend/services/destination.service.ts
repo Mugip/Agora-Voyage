fbf2731bb1c0ade43f25248f8ee7f55dec771bcc
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 21:31:41 +0300

    Create destination.service.ts

---
+++ frontend/services/destination.service.ts
@@ -0,0 +1,22 @@
+typescript
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DestinationService {
+  private apiUrl = 'http://example.com/api'; // Replace with your API endpoint
+
+  constructor(private http: HttpClient) {}
+
+  // Example API call to get destinations
+  getDestinations(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/destinations`);
+  }
+
+  // Example API call to add a destination
+  addDestination(destination: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/destinations`, destination);
+  }
