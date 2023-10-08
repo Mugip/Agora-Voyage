5d01c57830141bcc893a0e574e6e1b9b600f0c7e
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 13:16:27 +0300

    Update authentication.go

--- server/authentication.go
+++ server/authentication.go
@@ -1,1 +1,65 @@
+go
+package authentication
 
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+var jwtKey = []byte("your-secret-key") // Replace with your own secret key
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Perform user authentication here
+	validUser := authenticateUser(user.Username, user.Password)
+	if !validUser {
+		http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+		return
+	}
+
+	expirationTime := time.Now().Add(24 * time.Hour) // Token expiration time
+
+	// Create JWT claims
+	claims := &Claims{
+		Username: user.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	// Generate JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Set JWT token as response
+	w.Write([]byte(tokenString))
+}
+
+func authenticateUser(username, password string) bool {
+	// Perform user authentication logic here
+	// Return true if the provided username and password are valid, else false
+	return (username == "admin" && password == "password")
+}
