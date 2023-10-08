598c7543c2826d4466355bbb4ef4a9866cdc3c35
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 22:51:15 +0300

    Update app.module.ts

--- frontend/app.module.ts
+++ frontend/app.module.ts
@@ -1,1 +1,8 @@
 
+import {HTTP_INTERCEPTORS} from @angular/common/http ;
+// Add the `AuthInterceptor` to the `providers` array in the `@NgModule` decorator.  
+ {
+     provide: HTTP_INTERCEPTORS,
+     useClass: AuthInterceptor,
+     multi: true
+ }
