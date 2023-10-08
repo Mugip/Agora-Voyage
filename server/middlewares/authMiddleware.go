d4dc5e08fd8527ffffe477daa821a5bfb4aea941
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 13:02:59 +0300

    Update authMiddleware.go

--- server/middlewares/authMiddleware.go
+++ server/middlewares/authMiddleware.go
@@ -1,1 +1,34 @@
+go
+package middleware
 
+import (
+	"net/http"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Perform authentication logic here
+		if !isValidSession(r) {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// If authentication is successful, proceed to the next handler
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Example function to validate session
+func isValidSession(r *http.Request) bool {
+	// Retrieve session token from the cookie
+	sessionToken, err := r.Cookie("session")
+	if err != nil || sessionToken == nil {
+		return false
+	}
+
+	// TODO: Implement your session management logic
+	// Example: Check if session exists in the database and is not expired
+
+	// Placeholder implementation, always returns true
+	return true
+}
