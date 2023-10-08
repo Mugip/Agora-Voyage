d618f58c91c2026ed34f2bee112f442bfcf66d0f
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 12:35:29 +0300

    Update authHandlers.go

--- server/handlers/authHandlers.go
+++ server/handlers/authHandlers.go
@@ -1,1 +1,73 @@
+go
+package handlers
 
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type AuthHandler struct {
+	db *sql.DB
+}
+
+type UserCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type AuthResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
+func NewAuthHandler(db *sql.DB) *AuthHandler {
+	return &AuthHandler{db}
+}
+
+func (ah *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	var credentials UserCredentials
+	err := json.NewDecoder(r.Body).Decode(&credentials)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Failed to read request body: %v", err)
+		return
+	}
+
+	// TODO: Authenticate user credentials and generate access token
+
+	// Example login logic
+	if credentials.Username != "admin" || credentials.Password != "password" {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprint(w, "Invalid credentials")
+		return
+	}
+
+	accessToken := "some-access-token" // TODO: Generate a secure access token
+
+	response := AuthResponse{
+		AccessToken: accessToken,
+	}
+
+	jsonBytes, err := json.Marshal(response)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Failed to serialize response: %v", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func (ah *AuthHandler) Logout(w http.ResponseWriter, r *http.Request) {
+	// TODO: Implement logout logic (e.g., invalidate access token)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Logged out successfully")
+}
+
+// Add more methods as per your requirements
