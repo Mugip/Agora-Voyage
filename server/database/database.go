68a877543f55d3342931ee7d027b34fc9dd888e2
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 15:29:10 +0300

    Update database.go

--- server/database/database.go
+++ server/database/database.go
@@ -1,1 +1,85 @@
 
+go
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql" // MySQL driver
+)
+
+// Database represents the database connection
+type Database struct {
+	db *sql.DB
+}
+
+// NewDatabase creates a new database connection
+func NewDatabase() (*Database, error) {
+	db, err := sql.Open("mysql", "YOUR_DATABASE_CONNECTION_STRING")
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{db: db}, nil
+}
+
+// Close closes the database connection
+func (d *Database) Close() error {
+	return d.db.Close()
+}
+
+// RunMigrations runs all the required database migrations
+func (d *Database) RunMigrations() error {
+	migrations := []func(*sql.DB) error{
+		createUsersTable,
+		createPostsTable,
+		// Add other migration functions here
+	}
+
+	for _, migration := range migrations {
+		err := migration(d.db)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// createUsersTable creates the users table in the database
+func createUsersTable(db *sql.DB) error {
+	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id INT AUTO_INCREMENT PRIMARY KEY,
+			username VARCHAR(50) UNIQUE NOT NULL,
+			password VARCHAR(100) NOT NULL,
+			created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+		)
+	`)
+
+	return err
+}
+
+// createPostsTable creates the posts table in the database
+func createPostsTable(db *sql.DB) error {
+	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS posts (
+			id INT AUTO_INCREMENT PRIMARY KEY,
+			user_id INT NOT NULL,
+			title VARCHAR(100) NOT NULL,
+			content TEXT NOT NULL,
+			created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+			FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+		)
+	`)
+
+	return err
+}
+
+// Add other migration functions for additional tables here
