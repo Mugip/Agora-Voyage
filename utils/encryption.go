19ff72b998468ccf0481007c23145e0ee618485e
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-09 01:28:56 +0300

    Update encryption.go

--- utils/encryption.go
+++ utils/encryption.go
@@ -1,1 +1,57 @@
+go
+package utils
 
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+)
+
+// Encrypt encrypts the provided plaintext using AES encryption algorithm.
+// It returns the encrypted text encoded in base64.
+func Encrypt(key, plaintext string) (string, error) {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plaintext))
+
+	return base64.URLEncoding.EncodeToString(ciphertext), nil
+}
+
+// Decrypt decrypts the provided ciphertext using AES encryption algorithm.
+// It expects the ciphertext to be encoded in base64.
+func Decrypt(key, ciphertext string) (string, error) {
+	block, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		return "", err
+	}
+
+	decodedCiphertext, err := base64.URLEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return "", err
+	}
+
+	if len(decodedCiphertext) < aes.BlockSize {
+		return "", errors.New("ciphertext too short")
+	}
+
+	iv := decodedCiphertext[:aes.BlockSize]
+	decodedCiphertext = decodedCiphertext[aes.BlockSize:]
+
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(decodedCiphertext, decodedCiphertext)
+
+	return string(decodedCiphertext), nil
+}
