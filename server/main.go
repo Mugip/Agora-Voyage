7e405af8d09f8b443311eb347367ecf1b1526966
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 12:09:09 +0300

    Update main.go

--- server/main.go
+++ server/main.go
@@ -1,1 +1,31 @@
+go
+package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	// Define your API routes here
+	// Example:
+	// r.HandleFunc("/api/users", getUsers).Methods("GET")
+	// r.HandleFunc("/api/users/{id}", getUser).Methods("GET")
+
+	// Initialize your database connection here
+	// Example:
+	// db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/mydatabase")
+	// if err != nil {
+	//   log.Fatal(err)
+	// }
+
+	// Initialize any other services or dependencies here
+
+	// Start the server
+	log.Println("Server listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
