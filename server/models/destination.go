9684c78f93f4b491cd5b485ab20d82bbe0d50e28
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 14:19:29 +0300

    Update destination.go

--- server/models/destination.go
+++ server/models/destination.go
@@ -1,1 +1,264 @@
+go
+package models
 
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type Destination struct {
+	ID          int     `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	VisualURL   string  `json:"visualUrl"`
+	Location    string  `json:"location"`
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
+}
+
+type DestinationData []*Destination
+
+const openAIEndpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
+const unsplashEndpoint = "https://api.unsplash.com/photos/random"
+const mapEndpoint = "https://api.maps.com/geocode"
+
+var openAIKey = "your-openai-api-key"
+var unsplashKey = "your-unsplash-api-key"
+var mapKey = "your-map-api-key"
+
+var destinations = DestinationData{}
+
+func init() {
+	updateDestinationDescriptions()
+	retrieveDestinationInfo()
+}
+
+func updateDestinationDescriptions() {
+	for _, destination := range destinations {
+		if destination.Description == "" {
+			description, err := generateDestinationDescription(destination.Name)
+			if err != nil {
+				// Handle error
+				continue
+			}
+			destination.Description = description
+		}
+	}
+}
+
+func generateDestinationDescription(destinationName string) (string, error) {
+	// Create request payload for OpenAI API
+	payload := strings.NewReader(`{
+		"prompt": "You are planning a trip to ` + destinationName + `. Please describe this destination in an attractive and captivating way.",
+		"max_tokens": 50,
+		"temperature": 0.7
+	}`)
+
+	// Create a POST request to the OpenAI API
+	req, err := http.NewRequest("POST", openAIEndpoint, payload)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+openAIKey)
+
+	// Send the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Read the response
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// Parse the response to extract the generated description
+	openAIResponse := struct {
+		Choices []struct {
+			Text string `json:"text"`
+		} `json:"choices"`
+	}{}
+	if err := json.Unmarshal(respBody, &openAIResponse); err != nil {
+		return "", err
+	}
+
+	// Check if any description is generated
+	if len(openAIResponse.Choices) == 0 {
+		return "", errors.New("no description generated")
+	}
+
+	// Return the generated description
+	return openAIResponse.Choices[0].Text, nil
+}
+
+func retrieveDestinationInfo() {
+	for _, destination := range destinations {
+		if destination.VisualURL == "" {
+			visualURL, err := retrieveDestinationVisual(destination.Name)
+			if err != nil {
+				// Handle error
+				continue
+			}
+			destination.VisualURL = visualURL
+		}
+
+		if destination.Location == "" {
+			location, err := retrieveDestinationLocation(destination.Name)
+			if err != nil {
+				// Handle error
+				continue
+			}
+			destination.Location = location
+		}
+	}
+}
+
+func retrieveDestinationVisual(destinationName string) (string, error) {
+	// Create request URL for Unsplash API
+	url := unsplashEndpoint + "?query=" + destinationName
+
+	// Create a GET request to the Unsplash API
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("Accept-Version", "v1")
+	req.Header.Add("Authorization", "Client-ID "+unsplashKey)
+
+	// Send the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Read the response
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// Parse the response to extract the visual URL
+	unsplashResponse := struct {
+		URLs struct {
+			Regular string `json:"regular"`
+		} `json:"urls"`
+	}{}
+	if err := json.Unmarshal(respBody, &unsplashResponse); err != nil {
+		return "", err
+	}
+
+	// Check if visual URL is available
+	if unsplashResponse.URLs.Regular == "" {
+		return "", errors.New("no visual URL available")
+	}
+
+	// Return the visual URL
+	return unsplashResponse.URLs.Regular, nil
+}
+
+func retrieveDestinationLocation(destinationName string) (string, error) {
+	// Create request URL for Map API
+	url := mapEndpoint + "?query=" + destinationName
+
+	// Create a GET request to the Map API
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+mapKey)
+
+	// Send the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Read the response
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	// Parse the response to extract the location
+	mapResponse := struct {
+		Results []struct {
+			FormattedAddress string `json:"formatted_address"`
+		} `json:"results"`
+	}{}
+	if err := json.Unmarshal(respBody, &mapResponse); err != nil {
+		return "", err
+	}
+
+	// Check if location is available
+	if len(mapResponse.Results) == 0 {
+		return "", errors.New("no location information available")
+	}
+
+	// Return the location
+	return mapResponse.Results[0].FormattedAddress, nil
+}
+
+// Add a function to retrieve available luxury collections from an API
+func retrieveLuxuryCollections() ([]string, error) {
+	// Make the necessary API call to retrieve available luxury collections
+	// Implement the retrieval logic here
+
+	// Example response
+	luxuryCollections := []string{"Luxury Collection 1", "Luxury Collection 2", "Luxury Collection 3"}
+
+	return luxuryCollections, nil
+}
+
+func GetAllDestinations() DestinationData {
+	return destinations
+}
+
+// Update this function to retrieve destinations for a specific luxury collection
+func GetDestinationsByLuxuryCollection(luxuryCollectionName string) DestinationData {
+	filteredDestinations := DestinationData{}
+	for _, destination := range destinations {
+		// Filter destinations based on the provided luxury collection name
+		// Implement the filtering logic here
+	}
+	return filteredDestinations
+}
+
+// Update this function to create destinations automatically based on luxury collections
+func CreateDestinationsAutomatically() {
+	luxuryCollections, err := retrieveLuxuryCollections()
+	if err != nil {
+		// Handle error
+		return
+	}
+
+	for _, luxuryCollection := range luxuryCollections {
+		// Create destination based on the luxury collection
+		destination := &Destination{
+			ID:          getNextDestinationID(),
+			Name:        luxuryCollection,
+			Description: "",
+		}
+		destinations = append(destinations, destination)
+	}
+}
+
+func getNextDestinationID() int {
+	highestID := 0
+	for _, destination := range destinations {
+		if destination.ID > highestID {
+			highestID = destination.ID
+		}
+	}
+	return highestID + 1
+}
