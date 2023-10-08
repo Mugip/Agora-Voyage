dda450c927acdc9dfe5d6f481fd17229105323ca
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 13:11:36 +0300

    Update middlewares.go

--- server/middlewares.go
+++ server/middlewares.go
@@ -1,1 +1,27 @@
+go
+package middleware
 
+import (
+	"log"
+	"net/http"
+)
+
+func Middleware(authHandler http.Handler, errorHandler http.Handler) http.Handler {
+	return AuthMiddleware(ErrorMiddleware(AdditionalMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Add your custom middleware logic here
+		log.Println("Executing additional middleware logic")
+
+		// Call the authentication handler
+		authHandler.ServeHTTP(w, r)
+	}))))
+}
+
+func AdditionalMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Add your custom middleware logic here
+		log.Println("Executing additional middleware logic")
+
+		// Pass the request to the next handler
+		next.ServeHTTP(w, r)
+	})
+}
