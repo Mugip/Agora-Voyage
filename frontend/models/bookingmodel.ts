d3a364aa35493cb25c00ef0688974af879538363
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 17:28:08 +0300

    Create bookingmodel.ts

---
+++ frontend/models/bookingmodel.ts
@@ -0,0 +1,37 @@
+typescript
+export interface Booking {
+  id: number;
+  user_id: number;
+  start_time: Date;
+  end_time: Date;
+}
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookingService {
+  private apiUrl = 'http://your-api-url/bookings';
+
+  constructor(private http: HttpClient) { }
+
+  createBooking(userID: number, startTime: Date, endTime: Date): Observable<Booking> {
+    const booking: Booking = {
+      user_id: userID,
+      start_time: startTime,
+      end_time: endTime
+    };
+    return this.http.post<Booking>(this.apiUrl, booking);
+  }
+
+  getBookingByID(bookingID: number): Observable<Booking> {
+    const url = `${this.apiUrl}/${bookingID}`;
+    return this.http.get<Booking>(url);
+  }
+
+  // Add any additional methods as per your requirements
+
+}
