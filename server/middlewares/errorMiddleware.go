2a347097fcc69e3e7f620a86222a25e7957b6fd3
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 13:06:13 +0300

    Update errorMiddleware.go

--- server/middlewares/errorMiddleware.go
+++ server/middlewares/errorMiddleware.go
@@ -1,1 +1,19 @@
+go
+package middleware
 
+import (
+	"log"
+	"net/http"
+)
+
+func ErrorMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println("Internal Server Error:", err)
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
