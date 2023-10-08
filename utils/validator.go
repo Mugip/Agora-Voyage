b4c82635e713718814d0377fda7d78229dc4ceea
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-09 01:30:58 +0300

    Update validator.go

--- utils/validator.go
+++ utils/validator.go
@@ -1,1 +1,25 @@
+go
+package utils
 
+import (
+	"regexp"
+)
+
+// Validator defines a struct for input validation.
+type Validator struct {
+	regexEmail *regexp.Regexp
+}
+
+// NewValidator creates a new instance of Validator.
+func NewValidator() *Validator {
+	regexEmail := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
+
+	return &Validator{
+		regexEmail: regexEmail,
+	}
+}
+
+// ValidateEmail checks if the provided email is valid.
+func (v *Validator) ValidateEmail(email string) bool {
+	return v.regexEmail.MatchString(email)
+}
