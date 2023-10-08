c997cb8eafda00b77aa904b678a8e257f4da9074
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-08 21:34:38 +0300

    Update and rename reusable components here to destination.component.ts

---
+++ frontend/components/destination.component.ts
@@ -0,0 +1,42 @@
+typescript
+import { Component, OnInit } from '@angular/core';
+import { DestinationService } from '../services/destination.service';
+
+@Component({
+  selector: 'app-destination',
+  templateUrl: './destination.component.html',
+  styleUrls: ['./destination.component.css']
+})
+export class DestinationComponent implements OnInit {
+  destinations: any[] = [];
+
+  constructor(private destinationService: DestinationService) {}
+
+  ngOnInit(): void {
+    this.getDestinations();
+  }
+
+  getDestinations(): void {
+    this.destinationService.getDestinations().subscribe(
+      (response) => {
+        this.destinations = response;
+      },
+      (error) => {
+        console.log('An error occurred while fetching destinations:', error);
+      }
+    );
+  }
+
+  addDestination(destination: any): void {
+    this.destinationService.addDestination(destination).subscribe(
+      (response) => {
+        // Handle the response (if required)
+        console.log('Destination added successfully');
+        this.getDestinations(); // Refresh destinations after adding
+      },
+      (error) => {
+        console.log('An error occurred while adding a destination:', error);
+      }
+    );
+  }
+}
