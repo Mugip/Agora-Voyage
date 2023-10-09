88baab768886fe9833369db0981969cb2706987b
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-09 03:29:36 +0300

    Update httpclient.ts

--- frontend/services/httpclient.ts
+++ frontend/services/httpclient.ts
@@ -8,11 +8,27 @@ import { Destination } from './destinationmodel';
   providedIn: 'root'
 })
 export class DestinationService {
-  private apiUrl = 'http://localhost:8000/destinations';
+  private apiUrl = 'http://localhost:8000/destinations'; 
+  // replace with actual API of golang server
 
   constructor(private http: HttpClient) { }
 
   getDestinations(): Observable<Destination[]> {
     return this.http.get<Destination[]>(this.apiUrl);
   }
+}
+
+// luxury-travel.service.ts
+export class LuxuryTravelService {
+  private unsplashApiBaseUrl = 'https://api.unsplash.com';
+  private accessKey = '0se3s7GsJ_2q-8mk-BsW4H3BK5qW1FCBackbzzBcLNw';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getRandomDestinationImage(): Observable<any> {
+    const url = `${this.unsplashApiBaseUrl}/photos/random?query=destination&client_id=${this.accessKey}`;
+    return this.httpClient.get<any>(url);
+  }
+
+  // Add more methods to fetch images for other sections as needed
 }
