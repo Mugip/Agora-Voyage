68a500c8fae215b5a758491a3171bfafaa4f61d6
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-10 01:56:04 +0300

    Update app.module.ts

--- frontend/app.module.ts
+++ frontend/app.module.ts
@@ -1,8 +1,10 @@
 
-import {HTTP_INTERCEPTORS} from @angular/common/http ;
-// Add the `AuthInterceptor` to the `providers` array in the `@NgModule` decorator.  
- {
-     provide: HTTP_INTERCEPTORS,
-     useClass: AuthInterceptor,
-     multi: true
- }
+// {import {HTTP_INTERCEPTORS} from @angular/common/http ;
+// // Add the `AuthInterceptor` to the `providers` array in the `@NgModule` decorator.  
+// {
+//     provide: HTTP_INTERCEPTORS,
+//     useClass: AuthInterceptor,
+//     multi: true
+// }
+//}
+// ensure that the `ProtectedComponent` is also added to your `app.module.ts` file in the `declarations` array
