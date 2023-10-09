d3d0ec5b88e0f59a6c12219252747d1ef2172ae1
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-10 01:36:06 +0300

    Create login.component.ts

---
+++ frontend/components/login.component.ts
@@ -0,0 +1,44 @@
+typescript
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    const email = this.loginForm.controls.email.value;
+    const password = this.loginForm.controls.password.value;
+
+    this.http.post<any>('api/login', { email, password })
+      .subscribe(
+        response => {
+          // Handle successful login
+          console.log('Successfully logged in!');
+          // Store the JWT token received in the response, if applicable
+        },
+        error => {
+          // Handle login error
+          console.error('Login failed.');
+        }
+      );
+  }
