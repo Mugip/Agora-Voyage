a92cbad74d9fefe81a000d15cd45f98da7f92ca4
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-10 01:11:26 +0300

    Create app-routing.module.ts

---
+++ frontend/app-routing.module.ts
@@ -0,0 +1,19 @@
+javascript
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'protected', component: ProtectedComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
