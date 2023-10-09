a120b793cf42bc27200616833e9dbb6e34ce19bb
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-10 01:49:29 +0300

    Create protected.component.ts

---
+++ frontend/components/protected.component.ts
@@ -0,0 +1,20 @@
+typescript
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './authentication.go';
+
+@Component({
+  selector: 'app-protected',
+  templateUrl: './protected.component.html',
+  styleUrls: ['./protected.component.css']
+})
+export class ProtectedComponent implements OnInit {
+  user: any; // Placeholder for user data
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user = this.authService.getUser(); // Retrieve authenticated user data
+    // You can perform additional logic and retrieve data specific to the luxury travel app
+  }
+
+  // Add any other necessary methods and functionalities for the protected component
