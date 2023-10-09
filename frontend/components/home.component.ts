bcb034f6826d886eb18bbe176b41098c5b892b9e
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-10 01:22:51 +0300

    Create home.component.ts

---
+++ frontend/components/home.component.ts
@@ -0,0 +1,28 @@
+typescript
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'path/to/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  data: any;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    this.fetchData();  // Calling the fetchData method on component initialization
+  }
+
+  fetchData() {
+    this.apiService.getData().subscribe(
+      (response: any) => {
+        this.data = response;
+      },
+      (error: any) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
