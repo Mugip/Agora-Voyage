3f1e690d41649ac46661d05102f6369cf4d1bad1
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-10 22:26:14 +0300

    Update hotels.go

--- server/models/hotels.go
+++ server/models/hotels.go
@@ -15,7 +15,6 @@ var (
 	amadeusAPIKey   = os.Getenv("GfSs5vIeW0X2TmyawoWdlmPJie5VuMcT")      // Set your Amadeus API key here
 	openAIAPIKey    = os.Getenv("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")       // Set your OpenAI API key here
 	mapAPIKey       = os.Getenv("MAP_API_KEY")          // Set your Map API key here
-	openAISecretKey = os.Getenv("OPENAI_API_SECRET_KEY") // Set your OpenAI API Secret key here
 )
 
 type Hotel struct {
@@ -63,19 +62,19 @@ func getHotelsHandler(w http.ResponseWriter, r *http.Request) {
 func getHotelsFromAmadeusAPI() ([]Hotel, error) {
 	// Implement API call to Amadeus API here and parse the response
 	// Example code:
-	// resp, err := http.Get("https://api.amadeus.com/v2/hotels/search?...")
-	// if err != nil {
-	//     return nil, err
-	// }
-	// defer resp.Body.Close()
-	// // Parse the response and extract hotel information
-	// // Example:
-	// var hotels []Hotel
-	// err = json.NewDecoder(resp.Body).Decode(&hotels)
-	// if err != nil {
-	//     return nil, err
-	// }
-	// return hotels, nil
+	resp, err := http.Get("https://api.amadeus.com/v2/hotels/search?...")
+	if err != nil {
+	     return nil, err
+	 }
+	 defer resp.Body.Close()
+	// Parse the response and extract hotel information
+	// Example:
+	 var hotels []Hotel
+	 err = json.NewDecoder(resp.Body).Decode(&hotels)
+	 if err != nil {
+	     return nil, err
+	 }
+	 return hotels, nil
 
 	// Placeholder response for the sake of demonstration
 	return []Hotel{
@@ -100,18 +99,18 @@ func generateCatchyDescription(hotelName string) string {
 
 	// Implement API call to OpenAI API to generate catchy descriptions
 	// Example code:
-	// prompt := fmt.Sprintf("The %s is a luxurious hotel with breathtaking views", hotelName)
-	// response, err := openai.CreateCompletion(context.Background(), &openai.CreateCompletionRequest{
-	//     Model:   "text-davinci-003",
-	//     Prompt:  prompt,
-	//     MaxRerolls: 2,
-	//     ...
-	// })
-	// if err != nil {
-	//     return ""
-	// }
-	// description := response.Choices[0].Text
-	// return description
+	 prompt := fmt.Sprintf("The %s is a luxurious hotel with breathtaking views", hotelName)
+	 response, err := openai.CreateCompletion(context.Background(), &openai.CreateCompletionRequest{
+	     Model:   "text-davinci-003",
+	     Prompt:  prompt,
+	     MaxRerolls: 2,
+	     ...
+	 })
+	 if err != nil {
+	     return ""
+	 }
+	 description := response.Choices[0].Text
+	 return description
 
 	// Placeholder description for the sake of demonstration
 	return "Experience luxury like never before at the magnificent " + hotelName
@@ -120,28 +119,28 @@ func generateCatchyDescription(hotelName string) string {
 func getHotelLocationFromMapAPI(address string) (float64, float64, error) {
 	// Implement API call to Map API to retrieve the latitude and longitude for the given address
 	// Example code:
-	// resp, err := http.Get("https://maps.googleapis.com/maps/api/geocode/json?address=" + url.QueryEscape(address) + "&key=" + mapAPIKey)
-	// if err != nil {
-	//     return 0, 0, err
-	// }
-	// defer resp.Body.Close()
-	// // Parse the response and extract latitude and longitude
-	// // Example:
-	// var result struct {
-	//     Results []struct {
-	//         Geometry struct {
-	//             Location struct {
-	//                 Lat float64 `json:"lat"`
-	//                 Lng float64 `json:"lng"`
-	//             } `json:"location"`
-	//         } `json:"geometry"`
-	//     } `json:"results"`
-	// }
-	// err = json.NewDecoder(resp.Body).Decode(&result)
-	// if err != nil {
-	//     return 0, 0, err
-	// }
-	// return result.Results[0].Geometry.Location.Lat, result.Results[0].Geometry.Location.Lng, nil
+	 resp, err := http.Get("https://maps.googleapis.com/maps/api/geocode/json?address=" + url.QueryEscape(address) + "&key=" + mapAPIKey)
+	 if err != nil {
+	     return 0, 0, err
+	 }
+	 defer resp.Body.Close()
+	 // Parse the response and extract latitude and longitude
+	 // Example:
+	 var result struct {
+	     Results []struct {
+	         Geometry struct {
+	             Location struct {
+	                 Lat float64 `json:"lat"`
+	                 Lng float64 `json:"lng"`
+	             } `json:"location"`
+	         } `json:"geometry"`
+	     } `json:"results"`
+	 }
+	 err = json.NewDecoder(resp.Body).Decode(&result)
+	 if err != nil {
+	     return 0, 0, err
+	 }
+	 return result.Results[0].Geometry.Location.Lat, result.Results[0].Geometry.Location.Lng, nil
 
 	// Placeholder latitude and longitude for the sake of demonstration
 	return 40.7128, -74.0060, nil
