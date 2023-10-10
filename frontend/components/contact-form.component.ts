324c24993c194df22c2e6dfd60a8282b10166a4a
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-10 06:08:33 +0300

    Create contact-form.component.ts

---
+++ frontend/components/contact-form.component.ts
@@ -0,0 +1,30 @@
+typescript
+// contact-form.component.ts
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent {
+  contact = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  constructor(private http: HttpClient) {}
+
+  submitForm() {
+    this.http.post('/api/contact', this.contact).subscribe(
+      () => {
+        alert('Thank you for contacting us!');
+      },
+      (error) => {
+        console.error(error);
+        alert('Failed to submit the contact form. Please try again later.');
+      }
+    );
+  }
