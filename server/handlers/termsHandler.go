de473ae51e79374a1e5c0d20427c2696b130de93
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-10 06:17:21 +0300

    Create termsHandler.go

---
+++ server/handlers/termsHandler.go
@@ -0,0 +1,28 @@
+go
+// main.go
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func termsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Retrieve the terms and conditions content from a secure source (e.g., database or file)
+
+	termsContent := `These are our terms and conditions. Please read them carefully...`
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(termsContent))
+}
+
+func main() {
+	http.HandleFunc("/api/terms", termsHandler)
+	http.ListenAndServe(":8080", nil)
+}
