930de5a99527cbe4f667ec09c79dae7667056b45
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-10 05:52:06 +0300

    Create aboutHandler.go

---
+++ server/handlers/aboutHandler.go
@@ -0,0 +1,18 @@
+go
+// main.go
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/about.html"))
+	tmpl.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/about", aboutHandler)
+	http.ListenAndServe(":8080", nil)
+}
