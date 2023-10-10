9cfca81e1a6074bf8e6af133e8e21951ff9c22eb
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-10 05:50:09 +0300

    Create aboutRoutes.ts

---
+++ server/routes/aboutRoutes.ts
@@ -0,0 +1,15 @@
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  // Add other routes here
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
