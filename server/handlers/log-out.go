f4839847bb07932a1caec91be6cac2e212b55b8b
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-11 12:29:58 +0300

    Create log-out.go

---
+++ server/handlers/log-out.go
@@ -0,0 +1,36 @@
+go
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func logoutHandler(w http.ResponseWriter, r *http.Request) {
+	// Clear any stored tokens or user information from local storage
+	deleteTokenFromLocalStorage(r)
+
+	// Redirect the user to the login page or home page
+	http.Redirect(w, r, "/login", http.StatusSeeOther)
+}
+
+func deleteTokenFromLocalStorage(r *http.Request) {
+	// Retrieve the token from the request header or cookies
+	tokenString := extractTokenFromRequest(r)
+
+	// Verify and parse the token
+	token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte("SECRET_KEY"), nil // Replace with your secret key used for signing tokens
+	})
+
+	// Invalidate the token if needed and remove it from storage
+	// For example, you can set an expiry time for the token or maintain a blacklist of invalidated tokens
+	invalidateToken(token)
+}
+
+func invalidateToken(token *jwt.Token) {
+	// Implement your logic to invalidate the token
+	// This can involve updating the token in a database or adding it to a token blacklist
+} 
