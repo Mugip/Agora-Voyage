da3b1a22f28247d4402b98a4833c2ec146a09d53
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-11 16:26:39 +0300

    Create environment.prod.ts

---
+++ src/environment.prod.ts
@@ -0,0 +1,10 @@
+typescript
+// environment.prod.ts
+
+export const environment = {
+  production: true,
+  unsplashApiKey: 'your_unsplash_api_key',
+  amadeusApiKey: 'your_amadeus_api_key',
+  openAiApiKey: 'your_openai_api_key',
+  // Add any other production-specific environment variables here
+}
