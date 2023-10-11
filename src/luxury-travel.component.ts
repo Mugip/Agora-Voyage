48b568b45d2fccd272b9ebaa1e4f8e521955bfe7
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-11 16:10:52 +0300

    Create luxury-travel.component.ts

---
+++ src/luxury-travel.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-luxury-travel',
+  templateUrl: './luxury-travel.component.html',
+  styleUrls: ['./luxury-travel.component.css']
+})
+export class LuxuryTravelComponent implements OnInit {
+
+  // Add component logic and properties here
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Initialization logic
+  }
+}
