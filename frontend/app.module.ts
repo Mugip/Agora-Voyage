836557d91dfa1c016bae6510d1218a6be483544a
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-11 12:20:01 +0300

    Update app.module.ts

--- frontend/app.module.ts
+++ frontend/app.module.ts
@@ -1,10 +1,57 @@
+typescript
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import {HTTP_INTERCEPTORS} from '@angular/common/http' ;
 
-// {import {HTTP_INTERCEPTORS} from @angular/common/http ;
-// // Add the `AuthInterceptor` to the `providers` array in the `@NgModule` decorator.  
-// {
-//     provide: HTTP_INTERCEPTORS,
-//     useClass: AuthInterceptor,
-//     multi: true
-// }
-//}
-// ensure that the `ProtectedComponent` is also added to your `app.module.ts` file in the `declarations` array
+// Import components
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { BookingComponent } from './booking/booking.component';
+import { DestinationComponent } from './destination/destination.component';
+
+// Import services
+import { AuthService } from './core/auth/auth.service';
+import { BookingService } from './core/booking/booking.service';
+import { DestinationService } from './core/destination/destination.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent,
+    BookingComponent,
+    DestinationComponent,
+    ProtectedComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent },
+      { path: 'booking', component: BookingComponent },
+      { path: 'destination', component: DestinationComponent },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    ]),
+  ],
+  providers: [
+    AuthService,
+    BookingService,
+    DestinationService,
+    AuthInterceptor,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
+
