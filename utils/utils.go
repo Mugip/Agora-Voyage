027b320bc7396b4de5ea1beda779b2eca41ffaa7
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-12 14:02:14 +0300

    Update utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,39 @@
 package utils
 
-import (
-    "time"
-    "github.com/dgrijalva/jwt-go"
-    "golang.org/x/crypto/bcrypt"
-)
+import "github.com/dgrijalva/jwt-go"
+
+// AppError is a custom error type that provides additional information about application errors.
+type AppError struct {
+	Message string
+	Code    int
+}
+
+// Error returns the error message as a string.
+func (e *AppError) Error() string {
+	return e.Message
+}
+
+// NewAppError creates a new instance of AppError with the provided error message and code.
+func NewAppError(message string, code int) *AppError {
+	return &AppError{
+		Message: message,
+		Code:    code,
+	}
+}
+
+// IsAppError checks if the given error is an instance of AppError.
+func IsAppError(err error) bool {
+	_, ok := err.(*AppError)
+	return ok
+}
+
+// WrapError wraps the given error with additional context information.
+func WrapError(err error, message string, code int) *AppError {
+	return &AppError{
+		Message: message + ": " + err.Error(),
+		Code:    code,
+	}
+}
 
 // ErrNotFound is an app-specific error instance indicating a "not found" error.
 var ErrNotFound = NewAppError("Not Found", 404)
@@ -17,8 +46,8 @@ var ErrBadRequest = NewAppError("Bad Request", 400)
 
 // JWTManager defines a struct for JWT management.
 type JWTManager struct {
-    secretKey     []byte
-    tokenDuration time.Duration
+	secretKey     []byte
+	tokenDuration time.Duration
 }
 
 // NewJWTManager creates a new instance of JWTManager with the provided secret key and token duration.
