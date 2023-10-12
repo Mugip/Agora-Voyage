810f6a8ebfd14d71248eace104f273a5f26df5bc
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-12 13:49:52 +0300

    Update app_error.go

--- utils/app_error.go
+++ utils/app_error.go
@@ -2,33 +2,33 @@ package utils
 
 // AppError is a custom error type that provides additional information about application errors.
 type AppError struct {
-	Message string
-	Code    int
+    Message string
+    Code    int
 }
 
 // Error returns the error message as a string.
 func (e *AppError) Error() string {
-	return e.Message
+    return e.Message
 }
 
 // NewAppError creates a new instance of AppError with the provided error message and code.
 func NewAppError(message string, code int) *AppError {
-	return &AppError{
-		Message: message,
-		Code:    code,
-	}
+    return &AppError{
+        Message: message,
+        Code:    code,
+    }
 }
 
 // IsAppError checks if the given error is an instance of AppError.
 func IsAppError(err error) bool {
-	_, ok := err.(*AppError)
-	return ok
+    _, ok := err.(*AppError)
+    return ok
 }
 
 // WrapError wraps the given error with additional context information.
 func WrapError(err error, message string, code int) *AppError {
-	return &AppError{
-		Message: message + ": " + err.Error(),
-		Code:    code,
-	}
+    return &AppError{
+        Message: message + ": " + err.Error(),
+        Code:    code,
+    }
 }
