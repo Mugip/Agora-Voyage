a14904c22efc1eaf03ab4c5be61fe40d4aa774d8
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-12 13:55:03 +0300

    Update jwt.go

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,49 +1,14 @@
 package utils
 
 import (
-	"errors"
-	"fmt"
-	"time"
-
-	"github.com/dgrijalva/jwt-go"
+    "time"
+    "github.com/dgrijalva/jwt-go"
 )
 
 // JWTManager defines a struct for JWT management.
 type JWTManager struct {
-	secretKey     []byte
-	tokenDuration time.Duration
+    secretKey     []byte
+    tokenDuration time.Duration
 }
 
 // NewJWTManager creates a new instance of JWTManager with the provided secret key and token duration.
-func NewJWTManager(secretKey string, tokenDuration time.Duration) *JWTManager {
-	return &JWTManager{
-		secretKey:     []byte(secretKey),
-		tokenDuration: tokenDuration,
-	}
-}
-
-// GenerateToken generates a new JWT token with the provided claims.
-func (jm *JWTManager) GenerateToken(claims jwt.Claims) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	signedToken, err := token.SignedString(jm.secretKey)
-	if err != nil {
-		return "", fmt.Errorf("failed to sign JWT token: %w", err)
-	}
-	return signedToken, nil
-}
-
-// ParseToken parses and verifies the JWT token and returns the claims.
-func (jm *JWTManager) ParseToken(tokenString string, claims jwt.Claims) error {
-	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-		return jm.secretKey, nil
-	})
-	if err != nil {
-		return err
-	}
-
-	if !token.Valid {
-		return errors.New("invalid token")
-	}
-
-	return nil
-}
