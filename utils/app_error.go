3873654139782cfda79813ff3fe932c54fb6c139
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-12 13:53:51 +0300

    Update app_error.go

--- utils/app_error.go
+++ utils/app_error.go
@@ -32,3 +32,12 @@ func WrapError(err error, message string, code int) *AppError {
         Code:    code,
     }
 }
+
+// ErrNotFound is an app-specific error instance indicating a "not found" error.
+var ErrNotFound = NewAppError("Not Found", 404)
+
+// ErrUnauthorized is an app-specific error instance indicating an "unauthorized" error.
+var ErrUnauthorized = NewAppError("Unauthorized", 401)
+
+// ErrBadRequest is an app-specific error instance indicating a "bad request" error.
+var ErrBadRequest = NewAppError("Bad Request", 400)
