5fdd180fb30598e98d3746f34d4815b9e7206b94
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-13 09:43:41 +0300

    Update route-guards.ts

--- frontend/guards/route-guards.ts
+++ frontend/guards/route-guards.ts
@@ -1,6 +1,3 @@
-typescript
-// route-guards.ts
-
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from './auth.service'; 
