4dd4a5674e00cfd9312e9a4d5fb6ee186605741a
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-13 09:44:08 +0300

    Update auth.interceptors.ts

--- frontend/interceptors/auth.interceptors.ts
+++ frontend/interceptors/auth.interceptors.ts
@@ -1,4 +1,3 @@
-typescript
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
