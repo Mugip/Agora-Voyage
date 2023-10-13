e9e0b217d7b866c85c7e8619d02ec982d89f418b
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-13 09:40:24 +0300

    Update providers.ts

--- frontend/arrays/providers.ts
+++ frontend/arrays/providers.ts
@@ -1,4 +1,3 @@
-typescript
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from 'Agora-voyage/frontend/interceptors/auth.interceptor.ts'; // Replace with the actual path to your auth interceptor
