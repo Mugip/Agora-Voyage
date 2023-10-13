d0d02e661184b3597ffb24441d4b5459e5bd17a1
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-13 06:01:05 +0300

    Update karma.conf.ts

--- src/karma.conf.ts
+++ src/karma.conf.ts
@@ -2,30 +2,18 @@ import { ConfigOptions } from 'karma';
 
 module.exports = (config: ConfigOptions) => {
   config.set({
-    basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in the browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: (config.angularCli && config.angularCli.codeCoverage)
-      ? ['progress', 'coverage-istanbul']
-      : ['progress', 'kjhtml'],
+    browsers: ['Chrome'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO!,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
