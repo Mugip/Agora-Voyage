e79a01c7500b293f2dcde101cb0601879de5887b
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-15 10:12:21 +0300

    Update polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,48 +1,19 @@
-/***************************************************************************************************
-* POLYFILLS
-*/
-
-// This file includes polyfills needed by Angular and is loaded before the app.
-// You can add your necessary polyfills in this file.
-
-/***************************************************************************************************
-* Zone JS is required by Angular itself.
-*/
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
-/***************************************************************************************************
-* APPLICATION IMPORTS
-*/
-
-// Add your own polyfills below this line if needed.
-
-// Example polyfills for core-js (commonly used for older browsers support)
-// Uncomment the ones you need and install the corresponding npm package.
-
- import 'core-js/es/array';
- import 'core-js/es/date';
- import 'core-js/es/function';
- import 'core-js/es/map';
- import 'core-js/es/math';
- import 'core-js/es/number';
- import 'core-js/es/object';
- import 'core-js/es/parse-float';
- import 'core-js/es/parse-int';
- import 'core-js/es/reflect';
- import 'core-js/es/regexp';
- import 'core-js/es/set';
- import 'core-js/es/string';
- import 'core-js/es/symbol';
- import 'core-js/es/weak-map';
- import 'core-js/es/reflect';
-
-// Example polyfills for web-animations-js (required for animations)
-// Uncomment the import statement and install the corresponding npm package.
-
- import 'web-animations-js';   Run `npm install --save web-animations-js`.
-
-// Example polyfills for classlist.js (required for modern browsers' classlist support)
-// Uncomment the import statement and install the corresponding npm package.
-
- import 'classlist.js';   Run `npm install --save classlist.js`
+import 'zone.js/dist/zone';
+import 'core-js/es/array';
+import 'core-js/es/date';
+import 'core-js/es/function';
+import 'core-js/es/map';
+import 'core-js/es/math';
+import 'core-js/es/number';
+import 'core-js/es/object';
+import 'core-js/es/parse-float';
+import 'core-js/es/parse-int';
+import 'core-js/es/reflect';
+import 'core-js/es/regexp';
+import 'core-js/es/set';
+import 'core-js/es/string';
+import 'core-js/es/symbol';
+import 'core-js/es/weak-map';
+import 'core-js/es/reflect';
+import 'web-animations-js';   Run `npm install --save web-animations-js`
+import 'classlist.js';   Run `npm install --save classlist.js`
