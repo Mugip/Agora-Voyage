3e7a5f26e70b939501143bce5b4d125eb05d35ed
Mugip <141583223+Mugip@users.noreply.github.com>
2023-10-15 09:22:17 +0300

    Update karma.conf.ts

--- src/karma.conf.ts
+++ src/karma.conf.ts
@@ -1,8 +1,8 @@
-import { Config, Frameworks } from 'karma';
+import { Config } from 'karma';
 
 export default function(config: Config) {
   config.set({
-    frameworks: ['jasmine', '@angular-devkit/build-angular'] as Frameworks[],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
